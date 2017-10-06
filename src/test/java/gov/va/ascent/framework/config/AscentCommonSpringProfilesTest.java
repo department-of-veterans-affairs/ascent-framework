6502ee3ba3031487ceef1a810217eaba03d4d84e
prashanthmds <prashanthmds@gmail.com>
2017-10-06 14:29:14 +0200

    unit tests for ascent-framework

---
+++ src/test/java/gov/va/ascent/framework/config/AscentCommonSpringProfilesTest.java
@@ -0,0 +1,131 @@
+package gov.va.ascent.framework.config;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AscentCommonSpringProfilesTest {
+
+	/**
+	 * Spring default profile
+	 */
+	public static final String TEST_PROFILE_DEFAULT = "default";
+
+	/**
+	 * Spring profile for local dev environment
+	 */
+	public static final String TEST_PROFILE_ENV_LOCAL_INT = "local-int";
+
+	/**
+	 * Spring profile for local dev environment
+	 */
+	public static final String TEST_PROFILE_ENV_DOCKER_DEMO = "docker-demo";
+
+	/**
+	 * Spring profile for AWS CI environment
+	 */
+	public static final String TEST_PROFILE_ENV_AWS_CI = "aws-ci";
+
+	/**
+	 * Spring profile for AWS DEV environment
+	 */
+	public static final String TEST_PROFILE_ENV_AWS_DEV = "aws-dev";
+
+	/**
+	 * Spring profile for AWS STAGE environment
+	 */
+	public static final String TEST_PROFILE_ENV_AWS_STAGE = "aws-stage";
+
+	/**
+	 * Spring profile for AWS PROD environment
+	 */
+	public static final String TEST_PROFILE_ENV_AWS_PROD = "aws-prod";
+
+	/**
+	 * Spring profile for remote client real implementations
+	 */
+	public static final String TEST_PROFILE_REMOTE_CLIENT_IMPLS = "remote_client_impls";
+
+	/**
+	 * Spring profile for remote client simulator implementations
+	 */
+	public static final String TEST_PROFILE_REMOTE_CLIENT_SIMULATORS = "remote_client_sims";
+	
+	/**
+	 * Spring profile for unit test specific impls
+	 */
+	public static final String TEST_PROFILE_UNIT_TEST = "unit_test_sims";
+
+	/**
+	 * Spring profile for remote audit simulator implementations
+	 */
+	public static final String TEST_PROFILE_REMOTE_AUDIT_SIMULATORS = "remote_audit_client_sims";
+	
+	/**
+	 * Spring profile for remote audit impl implementations
+	 */
+	public static final String TEST_PROFILE_REMOTE_AUDIT_IMPLS = "remote_audit_client_impl";
+
+    @Test
+    public void profileDefaultTest() throws Exception {
+        assertEquals(TEST_PROFILE_DEFAULT, AscentCommonSpringProfiles.PROFILE_DEFAULT);
+    }
+    
+    @Test
+    public void profileLocalIntTest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_LOCAL_INT, AscentCommonSpringProfiles.PROFILE_ENV_LOCAL_INT);
+    }
+    
+    @Test
+    public void profileDockerDemoTest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_DOCKER_DEMO, AscentCommonSpringProfiles.PROFILE_ENV_DOCKER_DEMO);
+    }      
+    
+    @Test
+    public void profileAwsCITest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_AWS_CI, AscentCommonSpringProfiles.PROFILE_ENV_AWS_CI);
+    }
+    
+    @Test
+    public void profileAwsDevTest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_AWS_DEV, AscentCommonSpringProfiles.PROFILE_ENV_AWS_DEV);
+    }
+    
+    @Test
+    public void profileAwsStageTest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_AWS_STAGE, AscentCommonSpringProfiles.PROFILE_ENV_AWS_STAGE);
+    }
+    
+    @Test
+    public void profileAwsProdTest() throws Exception {
+        assertEquals(TEST_PROFILE_ENV_AWS_PROD, AscentCommonSpringProfiles.PROFILE_ENV_AWS_PROD);
+    }
+    
+    @Test
+    public void profileRemoteClientSimulatorsTest() throws Exception {
+        assertEquals(TEST_PROFILE_REMOTE_CLIENT_SIMULATORS, AscentCommonSpringProfiles.PROFILE_REMOTE_CLIENT_SIMULATORS);
+    }
+    
+    @Test
+    public void profileRemoteClientImplsTest() throws Exception {
+        assertEquals(TEST_PROFILE_REMOTE_CLIENT_IMPLS, AscentCommonSpringProfiles.PROFILE_REMOTE_CLIENT_IMPLS);
+    }    
+    
+    @Test
+    public void profileRemoteAuditSimulatorsTest() throws Exception {
+        assertEquals(TEST_PROFILE_REMOTE_AUDIT_SIMULATORS, AscentCommonSpringProfiles.PROFILE_REMOTE_AUDIT_SIMULATORS);
+    }
+    
+    @Test
+    public void profileRemoteAuditImplsTest() throws Exception {
+        assertEquals(TEST_PROFILE_REMOTE_AUDIT_IMPLS, AscentCommonSpringProfiles.PROFILE_REMOTE_AUDIT_IMPLS);
+    }   
+    
+    @Test
+    public void profileUnitTestingTest() throws Exception {
+        assertEquals(TEST_PROFILE_UNIT_TEST, AscentCommonSpringProfiles.PROFILE_UNIT_TEST);
+    }      
+}
---
+++ src/test/java/gov/va/ascent/framework/service/ServiceExceptionTest.java
@@ -0,0 +1,47 @@
+package gov.va.ascent.framework.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServiceExceptionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEmptyConstructor() {
+		ServiceException serviceException = new ServiceException();
+		assertNotNull(serviceException.getMessage());
+		assertNull(serviceException.getCause());
+	}
+
+	@Test
+	public void testConstructorWithStringAndThrowable() {
+		ServiceException serviceException = new ServiceException("Unit Testing", new Throwable());
+		assertNotNull(serviceException.getMessage());
+		assertNotNull(serviceException.getCause());
+	}
+
+	@Test
+	public void testContructorWithString() {
+		ServiceException serviceException = new ServiceException("Unit Testing");
+		assertNotNull(serviceException.getMessage());
+		assertNull(serviceException.getCause());
+	}
+
+	@Test
+	public void testConstructorWithThrowable() {
+		ServiceException serviceException = new ServiceException(new Throwable());
+		assertNotNull(serviceException.getMessage());
+		assertNotNull(serviceException.getCause());
+	}
+
+}
