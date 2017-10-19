bfd94944258e1293be711a52d47da1fae55123f1
Nate Paulus <npaulus@meetveracity.com>
2017-10-19 11:06:32 -0500

    DateAdapter testing logging

--- src/test/java/gov/va/ascent/framework/transfer/jaxb/adapters/DateAdapterTest.java
+++ src/test/java/gov/va/ascent/framework/transfer/jaxb/adapters/DateAdapterTest.java
@@ -1,24 +1,34 @@
 package gov.va.ascent.framework.transfer.jaxb.adapters;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.text.ParseException;
 import java.util.Calendar;
 import java.util.Date;
 
+import ch.qos.logback.classic.Logger;
+import gov.va.ascent.framework.aspect.AspectLoggingTestAppender;
+import gov.va.ascent.framework.aspect.PerformanceLoggingAspect;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class DateAdapterTest {
 
+	private Logger dateAdapterLog = (Logger) org.slf4j.LoggerFactory.getLogger(DateAdapter.class);
+
 	@Before
 	public void setUp() throws Exception {
+		DateAdapterLoggingTestAppender.events.clear();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		DateAdapterLoggingTestAppender.events.clear();
 	}
 
 	@Test
@@ -46,6 +56,15 @@ public class DateAdapterTest {
 		assertNotNull(retVal);
 		assertTrue(retVal.contains("-"));
 		assertTrue(retVal.contains(":"));
+	}
+
+	@Test
+	public void testParseDateTimeThrowsParseException() {
+
+		Date dt = DateAdapter.parseDateTime("1970-0101T00:00:00");
+		assertEquals("Error parsing date, returning null:1970-0101T00:00:00",
+				DateAdapterLoggingTestAppender.events.get(0).getMessage());
+		assertEquals(null, dt);
 	}
 
 }
