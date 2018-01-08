f44209b5156df5a11ebaf5b7d5c59f48f3bb63fc
Abhijit Kulkarni <akulkarni@meetveracity.com>
2018-01-08 15:39:17 -0500

    Added static final to constant variables for sonar violation fix

--- src/main/java/gov/va/ascent/framework/swagger/SwaggerResponseMessages.java
+++ src/main/java/gov/va/ascent/framework/swagger/SwaggerResponseMessages.java
@@ -5,30 +5,23 @@ package gov.va.ascent.framework.swagger;
  * the swagger documentation.  This is to ensure consistent messaging (and ideally behavior)
  * for services.
  * 
- * @author jshrader
+ * @author akulkarn
  */
-//jshrader interface constants is an approved pattern here, this rule should be disabled
+//akulkarn: interface constants is an approved pattern here, this rule should be disabled, but @SuppressWarnings doesn't work
 //CHECKSTYLE:OFF
-@SuppressWarnings({"squid:S1214","common-java:InsufficientLineCoverage"})
 public interface SwaggerResponseMessages {
-	//CHECKSTYLE:ON
-	
+//CHECKSTYLE:ON
 	/** The Constant MESSAGE_200. */
-	String MESSAGE_200 = "A Response which indicates a successful Request."
-			+ "  The Response may contain \"messages\" that could describe warnings or further information.";
+	static final String MESSAGE_200 = "A Response which indicates a successful Request.  The Response may contain \"messages\" that could describe warnings or further information.";
 	
 	/** The Constant MESSAGE_403. */
-	String MESSAGE_403 = "The request is not authorized.  Please verify credentials used in the request.";
+	static final String MESSAGE_403 = "The request is not authorized.  Please verify credentials used in the request.";
 	
 	/** The Constant MESSAGE_400. */
-	String MESSAGE_400 = "There was an error encountered processing the Request." 
-			+ "  Response will contain a  \"messages\" element that will provide further information on the error." 
-			+ "  This request shouldn't be retried until corrected.";
+	static final String MESSAGE_400 = "There was an error encountered processing the Request.  Response will contain a  \"messages\" element that will provide further information on the error.  This request shouldn't be retried until corrected.";
 	
 	/** The Constant MESSAGE_500. */
-	String MESSAGE_500 = "There was an error encountered processing the Request." 
-			+ "  Response will contain a  \"messages\" element that will provide further information on the error."
-			+ "  Please retry.  If problem persists, please contact support with a copy of the Response.";
+	static final String MESSAGE_500 = "There was an error encountered processing the Request.  Response will contain a  \"messages\" element that will provide further information on the error.  Please retry.  If problem persists, please contact support with a copy of the Response.";
 	
 
 }
