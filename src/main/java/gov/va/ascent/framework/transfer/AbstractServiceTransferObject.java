e726b18454e63838c38183e71e1bcaf67806b24d
armin <aburkholder@meetveracity.com>
2018-04-24 17:43:06 -0400

    ITF-17 AbstractTransferObject subclasses to differentiate between Service and Partner transfer objects

---
+++ src/main/java/gov/va/ascent/framework/transfer/AbstractServiceTransferObject.java
@@ -0,0 +1,12 @@
+package gov.va.ascent.framework.transfer;
+
+/**
+ * To be extended by any data transfer objects that will be generated in a service project.
+ *
+ * @author aburkholder
+ */
+public abstract class AbstractServiceTransferObject extends AbstractTransferObject {
+	private static final long serialVersionUID = 7818958501584142876L;
+
+	// TODO move AbstractTransferObject.validate() into this class
+}
