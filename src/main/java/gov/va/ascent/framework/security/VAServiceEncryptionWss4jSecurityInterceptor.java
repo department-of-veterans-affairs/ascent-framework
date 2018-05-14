58732fbbfebd414378fcc788b3b6d317bb8404b2
srikanthvanapalli <srikanthvanapalli@srikanths-mbp.fios-router.home>
2018-05-14 10:54:13 -0400

    plat-199 removed audit service code

--- src/main/java/gov/va/ascent/framework/security/VAServiceEncryptionWss4jSecurityInterceptor.java
+++ src/main/java/gov/va/ascent/framework/security/VAServiceEncryptionWss4jSecurityInterceptor.java
@@ -1,6 +1,5 @@
 package gov.va.ascent.framework.security;
 
-import gov.va.ascent.framework.security.AbstractEncryptionWss4jSecurityInterceptor;
 import org.apache.ws.security.WSSConfig;
 import org.apache.ws.security.WSSecurityException;
 import org.apache.ws.security.components.crypto.CryptoFactory;
@@ -8,15 +7,10 @@ import org.apache.ws.security.message.WSSecEncrypt;
 import org.apache.ws.security.message.WSSecHeader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.soap.SoapMessage;
 import org.w3c.dom.Document;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Date;
 
 /**
  * The Class VAServiceEncryptionWss4jSecurityInterceptor.
@@ -30,9 +24,6 @@ public class VAServiceEncryptionWss4jSecurityInterceptor extends AbstractEncrypt
 	/** The Constant LOGGER. */
 	private static final Logger LOGGER = LoggerFactory.getLogger(VAServiceEncryptionWss4jSecurityInterceptor.class);
 
-	/** The Constant REQUEST_CONTENT. */
-	private static final String REQUEST_CONTENT = "content";
-
 	/** Turns on/off auditing. */
 	@Value("${wss-framework-audit.enabled:true}")
 	private boolean auditEnabled;
@@ -41,36 +32,6 @@ public class VAServiceEncryptionWss4jSecurityInterceptor extends AbstractEncrypt
 	@Value("${wss-framework-audit.auditFullMessageContent:true}")
 	private boolean auditFullMessageContentsEnabled;
 
-	/** The audit service. */
-	@Autowired
-	private Audit auditService;
-
-	/**
-	 * Audit service information.
-	 * 
-	 * @param soapMessage the soap message
-	 * @param code the code
-	 */
-	private void auditServiceInformation(final SoapMessage soapMessage, final AuditEventTypeCode code) {
-		if (auditEnabled && auditFullMessageContentsEnabled) {
-
-			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			try {
-				soapMessage.writeTo(baos);
-
-				final LogEventRequest.AttributesBuilder attributes = new LogEventRequest.AttributesBuilder();
-				attributes.put(REQUEST_CONTENT, baos.toString(UTF_8));
-				final LogEventRequest logEventRequest =
-						new LogEventRequest(new Date(), code.getCode(), "Request call from our web service client", attributes);
-				auditService.logEvent(logEventRequest);
-				if (LOGGER.isDebugEnabled()) {
-					LOGGER.debug("value of " + code.getDescription() + " \n" + baos.toString(UTF_8));
-				}
-			} catch (final IOException e) {
-				LOGGER.error("i/o exception logging payload " + code.getDescription() + " ", e);
-			}
-		}
-	}
 
 	/*
 	 * (non-Javadoc)
@@ -100,7 +61,7 @@ public class VAServiceEncryptionWss4jSecurityInterceptor extends AbstractEncrypt
 			final WSSecHeader secHeader = new WSSecHeader();
 			secHeader.insertSecurityHeader(doc);
 
-			auditServiceInformation(soapMessage, AuditEventTypeCode.WS_REQUEST_UNENCRYPTED_AUDIT);
+			LOGGER.info("Log unencrypted Soap message: ß" + soapMessage.toString());
 			encrypt.setDocument(doc);
 
 			encrypt.build(doc, getCrypto(), secHeader);
@@ -129,15 +90,6 @@ public class VAServiceEncryptionWss4jSecurityInterceptor extends AbstractEncrypt
 		this.auditFullMessageContentsEnabled = auditFullMessageContentsEnabled;
 	}
 
-	/**
-	 * Sets the audit service.
-	 * 
-	 * @param auditService the new audit service
-	 */
-	public final void setAuditService(final AuditService auditService) {
-		this.auditService = auditService;
-	}
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -148,6 +100,6 @@ public class VAServiceEncryptionWss4jSecurityInterceptor extends AbstractEncrypt
 	@Override
 	protected final void validateMessage(final SoapMessage soapMessage, final MessageContext messageContext) {
 		super.validateMessage(soapMessage, messageContext);
-		auditServiceInformation(soapMessage, AuditEventTypeCode.WS_RESPONSE_UNENCRYPTED_AUDIT);
+		LOGGER.info(soapMessage.toString());
 	}
 }
