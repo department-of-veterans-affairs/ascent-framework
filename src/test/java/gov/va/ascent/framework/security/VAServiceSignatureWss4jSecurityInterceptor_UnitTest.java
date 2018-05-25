fe3985284a6983f676f41a432e74bb9de4373503
srikanthvanapalli <srikanthvanapalli@srikanths-mbp.fios-router.home>
2018-05-24 23:03:20 -0400

    plat-199 Fixing unit test cases

--- src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
+++ src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
@@ -4,6 +4,8 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.security.auth.callback.CallbackHandler;
@@ -13,11 +15,13 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ws.security.components.crypto.Crypto;
+import org.apache.ws.security.components.crypto.CryptoFactory;
 import org.apache.ws.security.saml.SAMLIssuerImpl;
 import org.apache.wss4j.common.saml.SAMLCallback;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.SoapMessage;
@@ -34,6 +38,36 @@ public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 	private static final String KEY_STORE_FILE = "src/test/resources/encryption/EFolderService/vbmsKeystore.jks";
 
 	private static final String SOAP_MESSAGE_FILE = "src/test/resources/testFiles/security/soapMessage.xml";
+	
+	/**
+	 * The security crypto provider
+	 */
+	@Value("${ascent-framework.org.apache.ws.security.crypto.provider}")
+	private String securityCryptoProvider;
+
+	/**
+	 * The security.crypto.merlin.keystore.type
+	 */
+	@Value("${ascent-framework.org.apache.ws.security.crypto.merlin.keystore.type}")
+	private String securityCryptoMerlinKeystoreType;
+	
+	/**
+	 * The security.crypto.merlin.keystore.password.
+	 */
+	@Value("${ascent-framework.org.apache.ws.security.crypto.merlin.keystore.password}")
+	private String securityCryptoMerlinKeystorePassword;
+	
+	/**
+	 * The security.crypto.merlin.keystore.alias
+	 */
+	@Value("${ascent-framework.org.apache.ws.security.crypto.merlin.keystore.alias}")
+	private String securityCryptoMerlinKeystoreAlias;
+	
+	/**
+	 * The securityCryptoMerlinKeystoreFile
+	 */
+	@Value("${ascent-framework.org.apache.ws.security.crypto.merlin.keystore.file}")
+	private String securityCryptoMerlinKeystoreFile;
 
 	@Mock
 	VAServiceSignatureWss4jSecurityInterceptor interceptor = new VAServiceSignatureWss4jSecurityInterceptor();
@@ -41,7 +75,8 @@ public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 	//@Test
 	public void testSecureMessage() throws Exception {
 		SoapMessage sm = WSInterceptorTestUtil.createSoapMessage(SOAP_MESSAGE_FILE);
-        interceptor.setCrypto(createCrypto());
+		Map<Object, Object> propsMap = retrieveCryptoProps();
+        interceptor.setCrypto(CryptoFactory.getInstance(Crypto.class, propsMap));
 		interceptor.secureMessage(sm, null);
 		Assert.assertTrue(sm.getSoapHeader()
 				.examineHeaderElements(new QName("Security")).hasNext());
@@ -54,6 +89,21 @@ public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 		
 		//Assert.assertEquals( Integer.parseInt(TTL_STR)*1000, DatatypeConverter.parseDate(strExpires).getTimeInMillis()-DatatypeConverter.parseDate(strCreated).getTimeInMillis());
 
+	}
+	
+
+	/**
+	 * Retrieves properties to set to create a crypto file
+	 * @return
+	 */
+	private Map<Object, Object> retrieveCryptoProps() {
+		Map<Object, Object> propsMap = new HashMap<Object, Object>();
+		propsMap.put("org.apache.ws.security.crypto.provider", securityCryptoProvider);
+		propsMap.put("org.apache.ws.security.crypto.merlin.keystore.type", securityCryptoMerlinKeystoreType);
+		propsMap.put("org.apache.ws.security.crypto.merlin.keystore.password", securityCryptoMerlinKeystorePassword);
+		propsMap.put("org.apache.ws.security.crypto.merlin.keystore.alias", securityCryptoMerlinKeystoreAlias);
+		propsMap.put("org.apache.ws.security.crypto.merlin.keystore.file", securityCryptoMerlinKeystoreFile);
+		return propsMap;
 	}
 	
 	private Crypto createCrypto() throws Exception {
