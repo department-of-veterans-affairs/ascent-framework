547a2b2a97f98744a34a7a96251e4d76b78036d9
srikanthvanapalli <srikanthvanapalli@srikanths-mbp.fios-router.home>
2018-05-28 23:02:28 -0400

    plat-219

--- src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
+++ src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
@@ -33,10 +33,8 @@ import org.xml.sax.SAXException;
 
 public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 
-	private static final String TRUST_STORE_FILE = "src/test/resources/encryption/EFolderService/vaebnTruststore.jks";
-	private static final String PROPERTIES_FILE = "apache-trusted-crypto.properties";
-	private static final String KEY_STORE_FILE = "src/test/resources/encryption/EFolderService/vbmsKeystore.jks";
 
+	private static final String PROPERTIES_FILE = "apache-trusted-crypto.properties";
 	private static final String SOAP_MESSAGE_FILE = "src/test/resources/testFiles/security/soapMessage.xml";
 	
 	/**
@@ -72,22 +70,24 @@ public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 	@Mock
 	VAServiceSignatureWss4jSecurityInterceptor interceptor = new VAServiceSignatureWss4jSecurityInterceptor();
 	
-	//@Test
+	@Test
 	public void testSecureMessage() throws Exception {
+		
 		SoapMessage sm = WSInterceptorTestUtil.createSoapMessage(SOAP_MESSAGE_FILE);
 		Map<Object, Object> propsMap = retrieveCryptoProps();
-        interceptor.setCrypto(CryptoFactory.getInstance(Crypto.class, propsMap));
+		Crypto crypto = CryptoFactory.getInstance(Crypto.class, propsMap);
+        interceptor.setCrypto(crypto);
+   
+        interceptor.setSecurementActions("Timestamp SAMLTokenSigned");
+        //interceptor.setSecurementSignatureCrypto(crypto);
+        interceptor.setSecurementUsername("selfsigned");
+        interceptor.setSecurementPassword("password");
+        interceptor.afterPropertiesSet();   
+        
 		interceptor.secureMessage(sm, null);
+		
 		Assert.assertTrue(sm.getSoapHeader()
 				.examineHeaderElements(new QName("Security")).hasNext());
-
-		/*String xml = WSInterceptorTestUtil.getRawXML(sm);
-		String strCreated = StringUtils.substringBetween(xml, "<wsu:Created>",
-				"</wsu:Created>");
-		String strExpires = StringUtils.substringBetween(xml, "<wsu:Expires>",
-				"</wsu:Expires>");*/
-		
-		//Assert.assertEquals( Integer.parseInt(TTL_STR)*1000, DatatypeConverter.parseDate(strExpires).getTimeInMillis()-DatatypeConverter.parseDate(strCreated).getTimeInMillis());
 
 	}
 	
@@ -105,43 +105,6 @@ public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 		propsMap.put("org.apache.ws.security.crypto.merlin.keystore.file", securityCryptoMerlinKeystoreFile);
 		return propsMap;
 	}
-	
-	private Crypto createCrypto() throws Exception {
 		
-        WebServiceTemplate template = new WebServiceTemplate();
-		SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory();
-		messageFactory.setSoapVersion(SoapVersion.SOAP_12);
-		messageFactory.afterPropertiesSet();
-		template.setMessageFactory(messageFactory);
-        Properties prop = new Properties();
-		prop.load(this.getClass().getResourceAsStream(PROPERTIES_FILE));
-		CryptoFactoryBean cryptoFactory = new CryptoFactoryBean();
-		//cryptoFactory.setKeyStoreLocation(new ClassPathResource(KEY_STORE_FILE));
-		cryptoFactory.setConfiguration(prop);
-		//cryptoFactory.setKeyStorePassword("password");
-		cryptoFactory.afterPropertiesSet();
-		Crypto crypto = (Crypto) cryptoFactory.getObject();
-	
-/*		SAMLIssuerImpl issuer = new SAMLIssuerImpl();
-		issuer.setIssuerCrypto(crypto);
-		issuer.setIssuerKeyName("selfsigned");
-		issuer.setIssuerKeyPassword("password");
-		issuer.setIssuerName("selfsigned");
-		issuer.setSendKeyValue(false);
-		issuer.setSignAssertion(true);
-		
-		Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
-		securityInterceptor.setSecurementActions("Timestamp SAMLTokenSigned");
-		securityInterceptor.setSecurementSignatureCrypto(crypto);
-		securityInterceptor.setSecurementUsername("selfsigned");
-		securityInterceptor.setSecurementPassword("password");
-		securityInterceptor.setSamlIssuer(issuer);
-		securityInterceptor.afterPropertiesSet();*/
-		
-		return crypto;
-
-	}
-
-	
 
 }
