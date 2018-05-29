9b2f6328dda7311859db33ae44b8cbc2731b8964
srikanthvanapalli <srikanthvanapalli@10.210.251.90>
2018-05-29 13:33:04 -0400

    plat-219

--- src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
+++ src/test/java/gov/va/ascent/framework/security/VAServiceSignatureWss4jSecurityInterceptor_UnitTest.java
@@ -1,36 +1,33 @@
 package gov.va.ascent.framework.security;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
-import javax.security.auth.callback.CallbackHandler;
-import javax.xml.bind.DatatypeConverter;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.commons.lang3.StringUtils;
 import org.apache.ws.security.components.crypto.Crypto;
 import org.apache.ws.security.components.crypto.CryptoFactory;
-import org.apache.ws.security.saml.SAMLIssuerImpl;
-import org.apache.wss4j.common.saml.SAMLCallback;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.ws.soap.SoapMessage;
-import org.springframework.ws.soap.SoapVersion;
-import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
-import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
-import org.springframework.ws.soap.security.wss4j2.support.CryptoFactoryBean;
-import org.xml.sax.SAXException;
+import gov.va.ascent.framework.config.AscentCommonSpringProfiles;
+import gov.va.ascent.framework.config.BaseYamlConfig;
 
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners(inheritListeners = false, listeners = { DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class, TransactionalTestExecutionListener.class })
+@ActiveProfiles({ AscentCommonSpringProfiles.PROFILE_REMOTE_CLIENT_SIMULATORS })
+@ContextConfiguration(inheritLocations = false, classes = { BaseYamlConfig.class})
 public class VAServiceSignatureWss4jSecurityInterceptor_UnitTest {
 
 
