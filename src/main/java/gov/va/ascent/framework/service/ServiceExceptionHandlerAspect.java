8f3a0d906db9f5dd057380d015648889d5dba624
armin <aburkholder@meetveracity.com>
2018-08-29 14:38:15 -0400

    rds-74 rest template

--- src/main/java/gov/va/ascent/framework/service/ServiceExceptionHandlerAspect.java
+++ src/main/java/gov/va/ascent/framework/service/ServiceExceptionHandlerAspect.java
@@ -10,38 +10,38 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import gov.va.ascent.framework.exception.ExceptionToExceptionTranslationHandler;
 import gov.va.ascent.framework.exception.AscentRuntimeException;
+import gov.va.ascent.framework.exception.ExceptionToExceptionTranslationHandler;
 import gov.va.ascent.framework.util.Defense;
 
 /**
- * The Class ServiceExceptionHandlerAspect is a exception handling aspect for the service tier.  The current implementation
- * leverages ExceptionToExceptionTranslator to basically translate and log errors.  So if exceptions are raised by a
+ * The Class ServiceExceptionHandlerAspect is a exception handling aspect for the service tier. The current implementation
+ * leverages ExceptionToExceptionTranslator to basically translate and log errors. So if exceptions are raised by a
  * web service client, a DAO, etc. and are of the type "HibernateException" or some 3rd party exception type, the exception
  * will pass through the translator which will ensure it gets logged and wrapped into a exception of our own type with
  * associated diagnostic details that we can leverage to help trace through exceptions later.
- * 
+ *
  * @author jshrader
  */
 @Aspect
 public class ServiceExceptionHandlerAspect extends BaseServiceAspect {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ServiceExceptionHandlerAspect.class);
 
-	private ExceptionToExceptionTranslationHandler exceptionToExceptionTranslator; 
-	
-    /**
-     * Instantiates a new service exception handler aspect using a default ExceptionToExceptionTranslator.
-     * Explicitly declare this bean and use the other constructor(s) to customize the translator.
-     */
-    public ServiceExceptionHandlerAspect() {
-    	//this to prevent double wrapping of AscentRuntimeException
-    	final Set<Class<? extends Throwable>> exclusionSet = new HashSet<>();
-    	exclusionSet.add(AscentRuntimeException.class);
-    	exclusionSet.add(IllegalArgumentException.class);
-    	this.exceptionToExceptionTranslator = new ExceptionToExceptionTranslationHandler(
-    			null, exclusionSet, ServiceException.class);
-    }
+	private ExceptionToExceptionTranslationHandler exceptionToExceptionTranslator;
+
+	/**
+	 * Instantiates a new service exception handler aspect using a default ExceptionToExceptionTranslator.
+	 * Explicitly declare this bean and use the other constructor(s) to customize the translator.
+	 */
+	public ServiceExceptionHandlerAspect() {
+		// this to prevent double wrapping of AscentRuntimeException
+		final Set<Class<? extends Throwable>> exclusionSet = new HashSet<>();
+		exclusionSet.add(AscentRuntimeException.class);
+		exclusionSet.add(IllegalArgumentException.class);
+		this.exceptionToExceptionTranslator = new ExceptionToExceptionTranslationHandler(
+				null, exclusionSet, ServiceException.class);
+	}
 
 	/**
 	 * Instantiates a new service exception handler aspect using the specified exception translator.
@@ -54,13 +54,20 @@ public class ServiceExceptionHandlerAspect extends BaseServiceAspect {
 		this.exceptionToExceptionTranslator = exceptionToExceptionTranslator;
 	}
 
+	/**
+	 * Translate exceptions at the Service Interface service response
+	 *
+	 * @param joinPoint
+	 * @param throwable
+	 * @throws Throwable
+	 */
 	@AfterThrowing(pointcut = "publicStandardServiceMethod()", throwing = "throwable")
 	public void afterThrowing(JoinPoint joinPoint, Throwable throwable) throws Throwable {
-		if(LOGGER.isDebugEnabled()){
-			LOGGER.debug("ServiceExceptionHandlerAspect executing, handling a throwable:" + joinPoint.toLongString());			
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("ServiceExceptionHandlerAspect executing, handling a throwable:" + joinPoint.toLongString());
 		}
-        exceptionToExceptionTranslator.handleViaTranslation(((MethodSignature) joinPoint.getStaticPart().getSignature()).getMethod(), 
-        		joinPoint.getArgs(), throwable);
-    }
-	
+		exceptionToExceptionTranslator.handleViaTranslation(((MethodSignature) joinPoint.getStaticPart().getSignature()).getMethod(),
+				joinPoint.getArgs(), throwable);
+	}
+
 }
