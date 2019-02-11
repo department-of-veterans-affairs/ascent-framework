454e86c83d8859e7ad071213616300fdd193e1a5
varun-akula <vakula@meetveracity.com>
2019-02-11 13:16:32 -0500

    DS-110 Reset formatting

--- src/test/java/gov/va/ascent/framework/util/AscentCacheUtilTest.java
+++ src/test/java/gov/va/ascent/framework/util/AscentCacheUtilTest.java
@@ -30,74 +30,79 @@ public class AscentCacheUtilTest {
 		result = AscentCacheUtil.checkResultConditions(serviceResponse);
 		assertFalse(result);
 	}
-
+	
 	@Test
 	public void testGetUserBasedKey() {
-		PersonTraits personTraits = new PersonTraits("user", "password", AuthorityUtils.createAuthorityList("ROLE_TEST"));
+		PersonTraits personTraits = new PersonTraits("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_TEST"));
 		personTraits.setPid("12345");
 		personTraits.setFirstName("firstName");
 		personTraits.setLastName("lastName");
-		Authentication auth =
-				new UsernamePasswordAuthenticationToken(personTraits, personTraits.getPassword(), personTraits.getAuthorities());
+		Authentication auth = new UsernamePasswordAuthenticationToken(personTraits,
+		personTraits.getPassword(), personTraits.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
-
+	
 	@Test
 	public void testGetUserBasedKey_fileNumber() {
-		PersonTraits personTraits = new PersonTraits("user", "password", AuthorityUtils.createAuthorityList("ROLE_TEST"));
+		PersonTraits personTraits = new PersonTraits("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_TEST"));
 		personTraits.setFileNumber("12345");
 		personTraits.setFirstName("firstName");
 		personTraits.setLastName("lastName");
-		Authentication auth =
-				new UsernamePasswordAuthenticationToken(personTraits, personTraits.getPassword(), personTraits.getAuthorities());
+		Authentication auth = new UsernamePasswordAuthenticationToken(personTraits,
+		personTraits.getPassword(), personTraits.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
-
+	
 	@Test
 	public void testGetUserBasedKey_pidZeroLength() {
-		PersonTraits personTraits = new PersonTraits("user", "password", AuthorityUtils.createAuthorityList("ROLE_TEST"));
+		PersonTraits personTraits = new PersonTraits("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_TEST"));
 		personTraits.setPid("");
 		personTraits.setFirstName("firstName");
 		personTraits.setLastName("lastName");
-		Authentication auth =
-				new UsernamePasswordAuthenticationToken(personTraits, personTraits.getPassword(), personTraits.getAuthorities());
+		Authentication auth = new UsernamePasswordAuthenticationToken(personTraits,
+		personTraits.getPassword(), personTraits.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
-
+		
 	@Test
 	public void testGetUserBasedKey_fileNumberZeroLength() {
-		PersonTraits personTraits = new PersonTraits("user", "password", AuthorityUtils.createAuthorityList("ROLE_TEST"));
+		PersonTraits personTraits = new PersonTraits("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_TEST"));
 		personTraits.setFileNumber("");
 		personTraits.setFirstName("firstName");
 		personTraits.setLastName("lastName");
-		Authentication auth =
-				new UsernamePasswordAuthenticationToken(personTraits, personTraits.getPassword(), personTraits.getAuthorities());
+		Authentication auth = new UsernamePasswordAuthenticationToken(personTraits,
+		personTraits.getPassword(), personTraits.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
-
+	
 	@Test
 	public void testGetUserBasedKey_fileNumberAndPidNull() {
-		PersonTraits personTraits = new PersonTraits("user", "password", AuthorityUtils.createAuthorityList("ROLE_TEST"));
+		PersonTraits personTraits = new PersonTraits("user", "password",
+				AuthorityUtils.createAuthorityList("ROLE_TEST"));
 		personTraits.setFirstName("firstName");
 		personTraits.setLastName("lastName");
-		Authentication auth =
-				new UsernamePasswordAuthenticationToken(personTraits, personTraits.getPassword(), personTraits.getAuthorities());
+		Authentication auth = new UsernamePasswordAuthenticationToken(personTraits,
+		personTraits.getPassword(), personTraits.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(auth);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
-
+	
 	@Test
 	public void testGetUserBasedKeyForNull() {
-		SecurityContextHolder.getContext().setAuthentication(null);
+		SecurityContextHolder.getContext ().setAuthentication (null);
 		String result = AscentCacheUtil.getUserBasedKey("test");
 		assertTrue(result.length() > 0);
 	}
