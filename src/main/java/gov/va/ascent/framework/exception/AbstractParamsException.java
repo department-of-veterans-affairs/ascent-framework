754fd835eb403af248f2eb6b0202293b84a123d4
armin <aburkholder@meetveracity.com>
2019-02-13 16:46:06 -0500

    DS-143 fix fortify, add comments

--- src/main/java/gov/va/ascent/framework/exception/AbstractParamsException.java
+++ src/main/java/gov/va/ascent/framework/exception/AbstractParamsException.java
@@ -4,6 +4,11 @@ package gov.va.ascent.framework.exception;
  * Custom extension of RuntimeException so that we can raise this for exceptions we have no intention
  * of handling and need to raise but for some reason (i.e. checkstyle checks) cannot raise
  * java's RuntimeException or allow the original exception to simply propagate.
+ * <p>
+ * This class can be extended to provide expression names and values used in exception messages
+ * that have replaceable parameters, e.g. "Some {0} message with {1} parameters".
+ * <p>
+ * Current use-case is for transmission of exceptions and messages back to JSR303 validation constraint messages.
  *
  * @author vanapalliv
  */
@@ -11,16 +16,15 @@ public class AbstractParamsException extends RuntimeException {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 2598842813684506349L;
-	
+
 	private Integer paramCount;
 
 	private String[] paramNames;
-	
+
 	private String[] paramValues;
 
-
 	/**
-	 * Instantiates a new service exception.
+	 * Instantiates a new service exception. Param Names and Values must be set separately.
 	 *
 	 * @param message the message
 	 * @param cause the cause
@@ -28,30 +32,62 @@ public class AbstractParamsException extends RuntimeException {
 	public AbstractParamsException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
-	
+
+	/**
+	 * An in-order array of replaceable parameter names used in the message.
+	 * These are the literal strings between the curly braces.
+	 *
+	 * @return String[] the names, in same order as thier respective getParamValues()
+	 */
 	public String[] getParamNames() {
 		return paramNames;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter names used in the message.
+	 * These are the literal strings between the curly braces.
+	 *
+	 * @param paramNames the names, in same order as thier respective getParamValues()
+	 */
 	public void setParamNames(String[] paramNames) {
 		this.paramNames = paramNames;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter values used in the message.
+	 * These are the values that replace the parameters in the message.
+	 *
+	 * @return String[] the values, in same order as their respective getParamNames()
+	 */
 	public String[] getParamValues() {
 		return paramValues;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter values used in the message.
+	 * These are the values that replace the parameters in the message.
+	 *
+	 * @param paramValues the values, in same order as their respective getParamNames()
+	 */
 	public void setParamValues(String[] paramValues) {
 		this.paramValues = paramValues;
-	}	
+	}
 
+	/**
+	 * Number of elements in the getParamNames() and getParamValues() arrays.
+	 *
+	 * @return Integer the number of elements in the name and value arrays
+	 */
 	public Integer getParamCount() {
 		return paramCount;
 	}
 
+	/**
+	 * Number of elements in the getParamNames() and getParamValues() arrays.
+	 *
+	 * @param paramCount the number of elements in the name and value arrays
+	 */
 	public void setParamCount(Integer paramCount) {
 		this.paramCount = paramCount;
 	}
-
-
 }
--- src/main/java/gov/va/ascent/framework/messages/AbstractMessage.java
+++ src/main/java/gov/va/ascent/framework/messages/AbstractMessage.java
@@ -6,6 +6,11 @@ import gov.va.ascent.framework.transfer.AbstractTransferObject;
  * Message is a generic abstraction of a "message" or "notification" which is layer agnostic and can be used to communicate status or
  * other sorts of information during method calls between components/layers. This is serializable and can be used in SOAP or REST
  * calls. This class has param names and values as lists.
+ * <p>
+ * This class can be extended to provide expression names and values used in messages
+ * that have replaceable parameters, e.g. "Some {0} message with {1} parameters".
+ * <p>
+ * Current use-case is for message transmission back to JSR303 validation constraint messages.
  *
  * @author vanapalliv
  */
@@ -13,14 +18,16 @@ public abstract class AbstractMessage extends AbstractTransferObject {
 
 	/** The Constant serialVersisonUID. */
 	private static final long serialVersionUID = -1711431368372127556L;
-	
+
 	private Integer paramCount = 0;
 
 	private String[] paramNames;
-	
+
 	private String[] paramValues;
-	
+
 	/**
+	 * Construct a message providing only replaceable parameters.
+	 * 
 	 * @param paramCount
 	 * @param paramNames
 	 * @param paramValues
@@ -31,34 +38,69 @@ public abstract class AbstractMessage extends AbstractTransferObject {
 		this.paramNames = paramNames;
 		this.paramValues = paramValues;
 	}
-	
+
+	/**
+	 * Construct default (empty) message object.
+	 */
 	public AbstractMessage() {
 		super();
 	}
 
+	/**
+	 * An in-order array of replaceable parameter names used in the message.
+	 * These are the literal strings between the curly braces.
+	 * 
+	 * @return String[] the names, in same order as thier respective getParamValues()
+	 */
 	public String[] getParamNames() {
 		return paramNames;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter names used in the message.
+	 * These are the literal strings between the curly braces.
+	 * 
+	 * @param paramNames the names, in same order as thier respective getParamValues()
+	 */
 	public void setParamNames(String[] paramNames) {
 		this.paramNames = paramNames;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter values used in the message.
+	 * These are the values that replace the parameters in the message.
+	 * 
+	 * @return String[] the values, in same order as their respective getParamNames()
+	 */
 	public String[] getParamValues() {
 		return paramValues;
 	}
 
+	/**
+	 * An in-order array of replaceable parameter values used in the message.
+	 * These are the values that replace the parameters in the message.
+	 * 
+	 * @param paramValues the values, in same order as their respective getParamNames()
+	 */
 	public void setParamValues(String[] paramValues) {
 		this.paramValues = paramValues;
 	}
 
+	/**
+	 * Number of elements in the getParamNames() and getParamValues() arrays.
+	 * 
+	 * @return Integer the number of elements in the arrays
+	 */
 	public Integer getParamCount() {
 		return paramCount;
 	}
 
+	/**
+	 * Number of elements in the getParamNames() and getParamValues() arrays.
+	 * 
+	 * @param paramCount the number of elements in the arrays
+	 */
 	public void setParamCount(Integer paramCount) {
 		this.paramCount = paramCount;
 	}
-	
-	
 }
